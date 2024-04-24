e974f606276441bb2ec5bfcbb596e7668af614c3
iamhamzamalik <ami.zeemalik@gmail.com>
2024-04-24 14:00:02 +0500

    Moved images down to avoid overlap with navbar

--- src/components/Project1/Project1.js
+++ src/components/Project1/Project1.js
@@ -1,88 +1,181 @@
 import React from "react";
-import pdfFile from "./your_pdf_file.pdf";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import img1 from "./1.jpeg";
-import img2 from "./2.jpeg";
-import img3 from "./3.jpeg";
-import img4 from "./4.jpeg";
-import img5 from "./5.jpeg";
-import img6 from "./6.jpeg";
-import img7 from "./7.jpeg";
-import img8 from "./8.jpeg";
+import img1 from "./Thesis fyp-01.png";
+import img2 from "./Thesis fyp-02.png";
+import img3 from "./Thesis fyp-03.png";
+import img4 from "./Thesis fyp-04.png";
+import img5 from "./Thesis fyp-05.png";
+import img6 from "./Thesis fyp-06.png";
+import img7 from "./Thesis fyp-07.png";
+import img8 from "./Thesis fyp-08.png";
+import img9 from "./Thesis fyp-09.png";
+import img10 from "./Thesis fyp-10.png";
+import img11 from "./Thesis fyp-11.png";
+import img12 from "./Thesis fyp-12.png";
+import img13 from "./Thesis fyp-13.png";
+import img14 from "./Thesis fyp-14.png";
+import img15 from "./Thesis fyp-15.png";
+import img16 from "./Thesis fyp-16.png";
+import img17 from "./Thesis fyp-17.png";
+import img18 from "./Thesis fyp-18.png";
+import img19 from "./Thesis fyp-19.png";
+import img20 from "./Thesis fyp-20.png";
+import img21 from "./Thesis fyp-21.png";
+import img22 from "./Thesis fyp-22.png";
+import img23 from "./Thesis fyp-23.png";
+import img24 from "./Thesis fyp-24.png";
+import img25 from "./Thesis fyp-25.png";
+import img26 from "./Thesis fyp-26.png";
+import img27 from "./Thesis fyp-27.png";
+import img28 from "./Thesis fyp-28.png";
+import img29 from "./Thesis fyp-29.png";
+import img30 from "./Thesis fyp-30.png";
+import img31 from "./Thesis fyp-31.png";
+import img32 from "./Thesis fyp-32.png";
+import img33 from "./Thesis fyp-33.png";
+import img34 from "./Thesis fyp-34.png";
+import img35 from "./Thesis fyp-35.png";
+import img36 from "./Thesis fyp-36.png";
+import img37 from "./Thesis fyp-37.png";
+const imageContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "20px",
+};
 
+const imageStyle = {
+  border: "1px solid #ccc",
+  padding: "20px",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  backgroundColor: "#fff",
+  margin: "20px",
+  width: "80%", // Adjust width as needed
+  maxWidth: "800px", // Set maximum width
+};
 function Project1() {
   return (
-    <div>
-      <div style={{ marginTop: "50px" }}>
-        <object
-          data={pdfFile}
-          type="application/pdf"
-          width="100%"
-          height="600px"
-        >
-          <embed src={pdfFile} type="application/pdf" />
-        </object>
+    // Container for all images
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "20px",
+        marginTop: "50px",
+      }}
+    >
+      {/* Each image wrapped with div for styling */}
+      <div style={imageContainerStyle}>
+        <img src={img1} alt="Image 1" style={imageStyle} />
       </div>
-
-      <Carousel>
-        <div>
-          <img
-            src={img1}
-            alt="Image 1"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-        <div>
-          <img
-            src={img2}
-            alt="Image 2"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-        <div>
-          <img
-            src={img3}
-            alt="Image 3"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-        <div>
-          <img
-            src={img4}
-            alt="Image 4"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-        <div>
-          <img
-            src={img5}
-            alt="Image 5"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-        <div>
-          <img
-            src={img6}
-            alt="Image 6"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-        <div>
-          <img
-            src={img7}
-            alt="Image 7"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-        <div>
-          <img
-            src={img8}
-            alt="Image 8"
-            style={{ maxWidth: "360px", maxHeight: "360px" }}
-          />
-        </div>
-      </Carousel>
+      <div style={imageContainerStyle}>
+        <img src={img2} alt="Image 2" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img3} alt="Image 3" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img4} alt="Image 4" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img5} alt="Image 5" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img6} alt="Image 6" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img7} alt="Image 7" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img8} alt="Image 8" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img9} alt="Image 9" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img10} alt="Image 10" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img11} alt="Image 11" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img12} alt="Image 12" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img13} alt="Image 13" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img14} alt="Image 14" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img15} alt="Image 15" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img16} alt="Image 16" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img17} alt="Image 17" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img18} alt="Image 18" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img19} alt="Image 19" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img20} alt="Image 20" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img21} alt="Image 21" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img22} alt="Image 22" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img23} alt="Image 23" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img24} alt="Image 24" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img25} alt="Image 25" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img26} alt="Image 26" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img27} alt="Image 27" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img28} alt="Image 28" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img29} alt="Image 29" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img30} alt="Image 30" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img31} alt="Image 31" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img32} alt="Image 32" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img33} alt="Image 33" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img34} alt="Image 34" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img35} alt="Image 35" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img36} alt="Image 36" style={imageStyle} />
+      </div>
+      <div style={imageContainerStyle}>
+        <img src={img37} alt="Image 37" style={imageStyle} />
+      </div>
     </div>
   );
 }
